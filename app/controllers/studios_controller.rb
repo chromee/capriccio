94c70cd7e9bcdea9fe5edb2f841aeb810c65313d
chromee <odk.mmmm@gmail.com>
2017-09-22 00:55:47 +0900

    無駄なコメント削除

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,28 +1,20 @@
 class StudiosController < ApplicationController
   before_action :set_studio, only: [:show, :edit, :update, :destroy]
 
-  # GET /studios
-  # GET /studios.json
   def index
     @studios = Studio.all
   end
 
-  # GET /studios/1
-  # GET /studios/1.json
   def show
   end
 
-  # GET /studios/new
   def new
     @studio = Studio.new
   end
 
-  # GET /studios/1/edit
   def edit
   end
 
-  # POST /studios
-  # POST /studios.json
   def create
     @studio = Studio.new(studio_params)
 
@@ -37,8 +29,6 @@ class StudiosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /studios/1
-  # PATCH/PUT /studios/1.json
   def update
     respond_to do |format|
       if @studio.update(studio_params)
@@ -51,8 +41,6 @@ class StudiosController < ApplicationController
     end
   end
 
-  # DELETE /studios/1
-  # DELETE /studios/1.json
   def destroy
     @studio.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class StudiosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_studio
       @studio = Studio.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def studio_params
       params.require(:studio).permit(:name)
     end
