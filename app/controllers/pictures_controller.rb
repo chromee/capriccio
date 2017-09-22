0556ced54ae43c4aacc55588dc7d6ffba11e4795
chromee <odk.mmmm@gmail.com>
2017-09-23 00:33:06 +0900

    色々見た目変更

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,7 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
   protect_from_forgery except: [:create]
+  before_action :set_animes, only: [:new, :edit, :create, :update]
 
   PER_PAGE = 30
 
@@ -15,6 +16,7 @@ class PicturesController < ApplicationController
   end
 
   def show
+    @anime = Anime.find_by_id(@picture.anime_id)
   end
 
   def new
@@ -29,6 +31,7 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.save
+        save_pictures_characters_relation(params[:character_ids].split(",")) if params[:character_ids].present?
         format.html { redirect_to @picture, notice: '画像がアップロードされまし' }
         format.json { render :show, status: :created, location: @picture }
       else
@@ -41,6 +44,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
+        save_pictures_characters_relation(params[:character_ids]) if params[:character_ids].present?
         format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @picture }
       else
@@ -63,12 +67,23 @@ class PicturesController < ApplicationController
       @picture = Picture.find(params[:id])
     end
 
+    def set_animes
+      @animes = Anime.all.order(:title)
+    end
+
     def picture_params
       if request.path_parameters[:format] == "json"
         json_request = ActionController::Parameters.new(JSON.parse(request.body.read))
-        json_request.require(:picture).permit(:name, :photo, :tag_list)
+        json_request.require(:picture).permit(:name, :photo, :tag_list, :anime_id)
       else
-        params.require(:picture).permit(:name, :photo, :tag_list)
+        params.require(:picture).permit(:name, :photo, :tag_list, :anime_id)
       end
     end
+
+    def save_pictures_characters_relation(character_ids)
+      character_ids.each do |character_id|
+        PicturesCharactersRelation.create(picture_id: @picture.id, character_id: character_id.to_i)
+      end
+    end
+
 end
