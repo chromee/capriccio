5e819d4b04c87053cc40aa27c015a10b14dc4218
chromee <odk.mmmm@gmail.com>
2017-09-23 01:35:46 +0900

    年・季節入力フォーム作成

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,2 +1,10 @@
 module PicturesHelper
+  def years_for_select
+    current_year = Time.now.year.to_i
+    years = []
+    current_year.downto(1) do |year|
+      years << year
+    end
+    years
+  end
 end
