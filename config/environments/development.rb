6fd24c4b26d8b846553d12c7d39297f5b297ee48
chromee <odk.mmmm@gmail.com>
2017-09-24 14:55:26 +0900

    設定ファイル変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  ActionController::Base.asset_host = "https://anicap.denx.jp"
 end
