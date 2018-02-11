a37dec80a0fadc18af6f23a958958af7b1d36890
chromee <odk.mmmm@gmail.com>
2018-02-11 20:37:16 +0900

    キャプチャから顔を切り出すメソッド作成

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -2,13 +2,11 @@ class Capture < ApplicationRecord
   require 'opencv'
   include OpenCV
 
-  attr_accessor :content_type, :original_filename, :image_data
-
   # 画像の設定
   has_attached_file :picture,
     styles: { large:"1000x1000", small: "300x300" } ,
-    url: "/pictures/:id/:style/:basename.:extension",
-    path: "#{Rails.root}/public/pictures/:id/:style/:basename.:extension"
+    url: "/pictures/captures/:id/:style/:basename.:extension",
+    path: "#{Rails.root}/public/pictures/captures/:id/:style/:basename.:extension"
   validates_attachment :picture,
     less_than: 20.megabytes,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
@@ -24,13 +22,22 @@ class Capture < ApplicationRecord
     Anime.find_by_id(self.anime_id)
   end
 
-  def gray_img
-    # face_detector = OpenCV::CvHaarClassifierCascade::load "#{Rails.root.to_s}/cascader/lbpcascade_animeface.xml"
-    path = "#{Rails.root.to_s}/public/assets/arts/#{self.id}"
-    img = CvMat.load("#{path}/original/#{self.picture_file_name}")
-    gray_img = img.BGR2GRAY
-    gray_img.save_image("#{path}/gray/#{self.picture_file_name}")
-    return "#{path}/gray/#{self.picture_file_name}"
+  def trimming_face(x, y, w, h, character_id=nil)
+    img = CvMat.load self.picture.path
+    face_pic = img.sub_rect x, y, w, h
+    face = Face.new(
+      capture_id: self.id,
+      character_id: character_id,
+      x: x, y: y, w: w, h: h,
+      is_face: true
+    )
+    tmp_path = "#{Rails.root}/tmp/#{self.picture.original_filename}"
+    face_pic.save_image tmp_path
+    File.open(tmp_path) do |pic|
+      face.picture = pic
+      face.save!
+    end
+    File.delete tmp_path
   end
 
   def self.create_from_file(img_path)
--- app/models/face.rb
+++ app/models/face.rb
@@ -1,2 +1,9 @@
 class Face < ApplicationRecord
+  
+  has_attached_file :picture,
+    url: "/pictures/faces/:id/:basename.:extension",
+    path: "#{Rails.root}/public/pictures/faces/:id_:basename.:extension"
+  validates_attachment :picture,
+    less_than: 20.megabytes,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
