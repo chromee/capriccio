a64a13666ce16a5f47c15ab9e301de3b2c1ec97e
chromee <odk.mmmm@gmail.com>
2018-05-02 18:51:12 +0900

    アプリ名を変えやすいように

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "あにきゃぷ"
+    base_title = app_name
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
+  end
+
+  def app_name
+    "Capp"
   end
 
 end
