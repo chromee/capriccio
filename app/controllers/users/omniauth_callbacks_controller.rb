09d5fff86945f44cdbfc95714e0bc236b77459af
chromee <odk.mmmm@gmail.com>
2018-05-03 00:14:27 +0900

    アカウント名を表示するように

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,6 +16,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     session[:oauth_token] = request.env['omniauth.auth']['credentials']['token']
     session[:oauth_token_secret] = request.env['omniauth.auth']['credentials']['secret']
+    session[:user_name] = request.env['omniauth.auth']['info']['name']
+    session[:user_nickname] = request.env['omniauth.auth']['info']['nickname']
 
     flash[:info] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
     sign_in_and_redirect @user, event: :authentication
