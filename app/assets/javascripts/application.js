06d21aa1616d7d97f01fa248467a2fbb1768a245
chromee <odk.mmmm@gmail.com>
2018-05-03 11:25:31 +0900

    turbolinks削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require jquery.turbolinks
 //= require popper
 //= require bootstrap-sprockets
 //= require bootstrap-tagsinput
