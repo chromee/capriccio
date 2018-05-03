55241651c7b701db31b0f5101cc4d07a6d156292
chromee <odk.mmmm@gmail.com>
2018-05-03 14:58:19 +0900

    anicap用処理削除

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -1,9 +1,6 @@
 class CapturesController < ApplicationController
   protect_from_forgery except: [:create]
   before_action :set_capture, only: [:show, :edit, :update, :destroy]
-  before_action :set_anime, only: [:show, :edit]
-  before_action :set_animes, only: [:new, :edit, :create, :update]
-  before_action :set_anime_id_to_params, only: [:create, :update]
 
   PER_PAGE = 30
 
@@ -30,7 +27,6 @@ class CapturesController < ApplicationController
     @capture = Capture.new(capture_params)
     respond_to do |format|
       if @capture.save
-        trimming_face
         flash[:info] = "capture uploaded."
         format.html { redirect_to @capture }
         format.json { render :show, status: :created, location: @capture }
@@ -45,7 +41,6 @@ class CapturesController < ApplicationController
   def update
     respond_to do |format|
       if @capture.update(capture_params)
-        trimming_face
         flash[:info] = "updated."
         format.html { redirect_to @capture }
         format.json { render :show, status: :ok, location: @capture }
@@ -71,38 +66,8 @@ class CapturesController < ApplicationController
       @capture = Capture.find(params[:id])
     end
 
-    def set_anime
-      @anime = @capture.anime
-    end
-
-    def set_animes
-      @animes = Anime.all.order(:title)
-    end
-
-    def set_anime_id_to_params
-      return if params[:anime_title].blank?
-      unless anime = Anime.find_by_title(params[:anime_title])
-        return flash[:danger] = "anime is not found"
-      end
-      params[:capture][:anime_id] = anime.id
-    end
-
     def capture_params
       params.require(:capture).permit(:name, :picture, :tag_list, :anime_id)
-    end
-
-    def trimming_face
-      return if params[:characters].blank?
-      caharacters_name = params[:characters].values.map {|c| c[:name] }
-      characters = Character.where(name: caharacters_name).to_a
-      results = []
-      params[:characters].values.each do |character|
-        character.each {|k,v| character[k] = v.to_i unless k == :character_id }
-        character[:character_id] = characters.find {|c| c.name == character[:name] }.try(:id)
-        result, message = @capture.trimming_face! character
-        results << { result:result, msg: message }
-      end
-      flash[:danger] = results.map{|r| r[:msg]}.to_s if results.map{|r| r[:result]}.include?(false)
     end
 
 end
