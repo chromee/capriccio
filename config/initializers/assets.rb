fc9284c061d8c2349ceeb36134a45b80644d3973
chromee <odk.mmmm@gmail.com>
2020-03-23 01:24:16 +0900

    some fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,10 +10,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+return if ENV.fetch("SPROCKETS_CACHE").nil?
 
 Rails.application.config.assets.configure do |env|
-  return if ENV.fetch("SPROCKETS_CACHE").nil?
-
   env.cache = Sprockets::Cache::FileStore.new(
     ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
     Rails.application.config.assets.cache_limit,
